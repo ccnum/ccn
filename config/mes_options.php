5c8595c49f431ba2afbb73aa77df9cf244b0820d
pierretux <webmaster@pierre-kuhn.org>
2024-08-23 14:48:32 +0200

    feat: rajout de debug pour les logs

--- config/mes_options.php
+++ config/mes_options.php
@@ -13,6 +13,11 @@ if (preg_match('/ddev.site/', $_SERVER['HTTP_HOST'])) {
 	define('_TEST_EMAIL_DEST', 'pierrekuhn@ik.me');
 } else {
 	$GLOBALS['spip_header_silencieux'] = 1;
+	define('_NO_CACHE', -1);
+	define('_INTERDIRE_COMPACTE_HEAD_ECRIRE', true);
+	error_reporting(E_ALL ^ E_NOTICE);
+	ini_set("display_errors", "On");
+	define('SPIP_ERREUR_REPORT', E_ALL);
 }
 
 define('_DOC_MAX_SIZE', 3000);
