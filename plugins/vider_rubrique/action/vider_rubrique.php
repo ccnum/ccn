b05311f2a25115698b3af462719d9c6c3cd1b1fe
DDEV User <nobody@example.com>
2024-09-02 11:24:11 +0000

    Ajout du plugin 'vider_rubrique' : v0.4.0

---
+++ plugins/vider_rubrique/action/vider_rubrique.php
@@ -0,0 +1,45 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2012                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+ * \***************************************************************************/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/charsets');    # pour le nom de fichier
+include_spip('inc/utils');
+
+/**
+ * Effacer le contenu d'une rubrique
+ *
+ * @param null $arguments Possibilité d'avoir X ou X:vider_arbo.
+ * @return void
+ */
+function action_vider_rubrique_dist($arguments = null) {
+	if (is_null($arguments)) {
+		$securiser_action = charger_fonction('securiser_action', 'inc');
+		$arguments = $securiser_action();
+	}
+
+	$message = "Suppression des articles de la rubrique ";
+	$arguments = explode(':', $arguments);
+	$id_rubrique = $arguments[0];
+	$vider_arbo = '';
+	if (isset($arguments[1])) {
+		$vider_arbo = $arguments[1];
+		$message = "Suppression de l'arborescence complète de la rubrique ";
+	}
+	spip_log($message . $id_rubrique, "vider_rubrique");
+	if (intval($id_rubrique)) {
+		$contexte = array('id_rubrique' => $id_rubrique, 'vider_arbo' => $vider_arbo);
+		$suppression = recuperer_fond("admin/vider_rubrique", $contexte);
+	}
+}
