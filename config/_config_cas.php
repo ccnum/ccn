d540952a55e5214fb660300a51d52589834f81ee
pierretux <webmaster@pierre-kuhn.org>
2024-09-03 16:05:49 +0200

    fix: mettre en place le cicas correctement

---
+++ config/_config_cas.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS['ciconfig']['cicas'] = 'hybride';
+$GLOBALS['ciconfig']['cicasuid'] = 'login';
+$GLOBALS['ciconfig']['cicasurldefaut'] = 'www.laclasse.com';
+$GLOBALS['ciconfig']['cicasrepertoire'] = '/sso';
+$GLOBALS['ciconfig']['cicasport'] = '443';
+$GLOBALS['ciconfig']['cicas_creer_auteur'] = '';
+$GLOBALS['ciconfig']['cicashostordre'] = ['SERVER_NAME', 'HTTP_HOST', 'HTTP_X_FORWARDED_SERVER'];
