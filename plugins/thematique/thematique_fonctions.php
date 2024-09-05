07b144c864c9b4ec77106f6145acf053c1b1eb1c
pierretux <webmaster@pierre-kuhn.org>
2024-09-05 16:29:50 +0200

    feat: rajouter la balise ANNE_SCOLAIRE

--- plugins/thematique/thematique_fonctions.php
+++ plugins/thematique/thematique_fonctions.php
@@ -1,5 +1,18 @@
 <?php
 
+function balise_ANNEE_SCOLAIRE_dist($p) {
+	if ((isset($_GET['annee_scolaire'])) && ($_GET['annee_scolaire'] != 0) && ($_GET['annee_scolaire'] != '')) {
+		$p->code = $_GET['annee_scolaire'];
+	} else {
+		if (date('m') >= 8) {
+			$p->code = date('Y');
+		} else {
+			$p->code = date('Y') - 1;
+		}
+	}
+	return $p;
+}
+
 /* Surcharges Fonctions plugins
 function formulaires_joindre_document_traiter(
 	$id_document = 'new',
