7cb115604fd44faf570794f39f210ca12e394ed2
pierretux <webmaster@pierre-kuhn.org>
2024-09-09 11:49:56 +0200

    feat: passer le host en premier

--- config/_config_cas.php
+++ config/_config_cas.php
@@ -5,4 +5,4 @@ $GLOBALS['ciconfig']['cicasurldefaut'] = 'www.laclasse.com';
 $GLOBALS['ciconfig']['cicasrepertoire'] = '/sso';
 $GLOBALS['ciconfig']['cicasport'] = '443';
 $GLOBALS['ciconfig']['cicas_creer_auteur'] = '';
-$GLOBALS['ciconfig']['cicashostordre'] = ['SERVER_NAME', 'HTTP_HOST', 'HTTP_X_FORWARDED_SERVER'];
+$GLOBALS['ciconfig']['cicashostordre'] = ['HTTP_HOST', 'HTTP_X_FORWARDED_SERVER', 'SERVER_NAME'];
