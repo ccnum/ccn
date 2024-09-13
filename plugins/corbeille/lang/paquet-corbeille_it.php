4a9715cf8529b70d7d4e499809fe8e1bc2ac9c0c
DDEV User <nobody@example.com>
2024-09-13 07:36:13 +0000

    Ajout du plugin 'corbeille' : v4.1.0

---
+++ plugins/corbeille/lang/paquet-corbeille_it.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-corbeille?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'corbeille_description' => 'Questo plugin permette di derogare al comportamento standard di SPIP che elimina automaticamente alcuni oggetti dal database (articoli, notizie, ecc. nel cestino), con un ritardo di uno o due giorni.
+
+Con questo plugin, nessun altro oggetto viene cancellato automaticamente. Il cestino conserva tutti gli oggetti o altri oggetti che ci metti dentro e puoi sempre tornare indietro.',
+	'corbeille_nom' => 'Cestino',
+	'corbeille_slogan' => 'Gestione degli oggetti nel cestino.'
+);
