36d8010d3d9deca025a3324559d64984609b845d
pierretux <webmaster@pierre-kuhn.org>
2024-09-16 14:42:59 +0200

    feat: début de la refont de l'import de csv

---
+++ plugins/thematique/lang/thematique_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = [
+
+	'bouton_previsu_importer' => 'Prévisualiser l\'import',
+	'cartable_ligne' => 'Mon cartable en ligne',
+	'label_file_import' => 'Fichier à importer',
+	'titre_page_importer_user' => 'Importe des utilisateurs'
+];
