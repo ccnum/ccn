267d5bc48f8433bf9057e4623602aa02228db59b
pierretux <webmaster@pierre-kuhn.org>
2024-10-02 09:00:01 +0200

    feat: faire un plugins qui gère les imports utilisateurs distinct

---
+++ plugins/imports_utilisateurs/lang/imports_utilisateurs_fr.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+return [
+
+	'bouton_previsu_importer' => 'Prévisualiser l\'import',
+	'bouton_importer' => 'Faire l\'import',
+	'cartable_ligne' => 'Mon cartable en ligne',
+	'info_1_auteur_importer' => 'Vous allez importer 1 auteur',
+	'info_nb_auteurs_importer' => 'Vous allez importer @nb@ auteurs',
+	'info_aucun_auteurs_importer' => 'Vous n\'avez aucun auteur à importer',
+	'label_file_import' => 'Fichier à importer',
+	'titre_page_importer_user' => 'Import des utilisateurs'
+];
