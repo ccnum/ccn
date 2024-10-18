9c8c8e5794d5a2c0490496666b576ed27e8a63e0
DDEV User <nobody@example.com>
2024-10-18 12:50:08 +0000

    Ajout du plugin 'oembed' : v3.3.0

---
+++ plugins/oembed/lang/oembed_it.php
@@ -0,0 +1,49 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/oembed?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_provider' => 'Nessun provider',
+
+	// B
+	'bouton_lecture' => 'Lettura',
+
+	// E
+	'erreur_ajout_provider' => 'Errore durante l’aggiunta del provider.',
+	'erreur_scheme_doublon' => 'Questo schema è già presente nell’elenco.',
+	'explication_scheme' => 'usa * come carattere jolly',
+	'explication_upload_url' => 'Inserisci semplicemente l’URL della pagina che contiene il documento per <a href="https://contrib.spip.net/4595"> i siti più popolari</a>.',
+
+	// I
+	'info_1_provider' => '1 provider',
+	'info_nb_providers' => '@nb@ provider',
+
+	// L
+	'label_detecter_lien' => 'Rileva automaticamente i provider (ignora la whitelist)?',
+	'label_embed_auto' => 'Trasforma automaticamente gli URL inseriti nel testo?',
+	'label_endpoint' => 'Endpoint',
+	'label_inserer_head' => 'Il sito stesso è un provider oEmbed',
+	'label_maxheight' => 'Altezza massima predefinita (px)',
+	'label_maxwidth' => 'Larghezza massima predefinita (px)',
+	'label_scheme' => 'Schema',
+
+	// O
+	'ok_ajout_provider' => 'Provider aggiunto',
+
+	// S
+	'supprimer_provider' => 'Eliminare',
+
+	// T
+	'titre_ajouter_provider' => 'Aggiungi un provider',
+	'titre_configuration_avancee_providers' => 'Accedi alla configurazione avanzata dei provider oEmbed',
+	'titre_configurer_oembed' => 'Configura oEmbed',
+	'titre_configurer_providers' => 'Provider oEmbed',
+	'titre_oembed' => 'oEmbed'
+);
