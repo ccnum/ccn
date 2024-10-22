319138ff0814fd62bb7a6ae78578dd8c604b562f
pierretux <webmaster@pierre-kuhn.org>
2024-10-22 09:23:01 +0200

    feat(config): écrire les fichiers de config au up

--- config/_config_cas.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS['ciconfig']['cicas'] = 'hybride';
-$GLOBALS['ciconfig']['cicasuid'] = 'login';
-$GLOBALS['ciconfig']['cicasurldefaut'] = 'www.laclasse.com';
-$GLOBALS['ciconfig']['cicasrepertoire'] = '/sso';
-$GLOBALS['ciconfig']['cicasport'] = '443';
-$GLOBALS['ciconfig']['cicas_creer_auteur'] = '6forum';
-$GLOBALS['ciconfig']['cicashostordre'] = ['HTTP_HOST', 'SERVER_NAME', 'HTTP_X_FORWARDED_SERVER'];
