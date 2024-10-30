25e18404d9ab3e2be8fb96c81f4b056cf9e3fc52
pierretux <webmaster@pierre-kuhn.org>
2024-10-30 08:58:30 +0100

    fix: liste des plugins

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -103,19 +103,20 @@ if [[ ! -e config/connect.php && ${SPIP_AUTO_INSTALL} = 1 ]]; then
         # Optionally handle this error differently, or just continue
     fi
 
-	spip plugins:activer oembed -y
-	spip plugins:activer saisies -y
-	spip plugins:activer yaml -y
 	spip plugins:activer cextras -y
+	spip plugins:activer cicas -y
 	spip plugins:activer crayons -y
+	spip plugins:activer corbeille -y
 	spip plugins:activer facteur -y
+	spip plugins:activer imports_utilisateurs -y
 	spip plugins:activer jqueryui -y
 	spip plugins:activer notation -y
 	spip plugins:activer notifications -y
+	spip plugins:activer oembed -y
+	spip plugins:activer saisies -y
 	spip plugins:activer socialtags -y
 	spip plugins:activer spip_bonux -y
-	spip plugins:activer cicas -y
-	spip plugins:activer corbeille -y
+	spip plugins:activer yaml -y
 	if [ ${SPIP_VERSION_SITE} = "fictions" ]; then
 		spip plugins:activer vider_rubrique -y
 	fi
