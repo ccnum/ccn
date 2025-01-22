1f7c4e4598050478da4f8106a50a1b0d38befa6c
pierretux <webmaster@pierre-kuhn.org>
2025-01-22 15:26:44 +0100

    feat(prologue): amélioré la génération des histoire à partir des prologue

--- plugins/petitfablab/squelettes/formulaires/creer_histoire.php
+++ plugins/petitfablab/squelettes/formulaires/creer_histoire.php
@@ -10,30 +10,24 @@ include_spip('action/editer_rubrique');
 include_spip('inc/autoriser');
 
 function formulaires_creer_histoire_saisies_dist($rub_parent, $retour = '') {
+	$prologues = $defaut = [];
+	$articles = sql_allfetsel(
+		'a.id_article,a.titre,c.nom',
+		'spip_articles AS a JOIN spip_auteurs_liens AS b JOIN spip_auteurs AS c ON (a.id_article=b.id_objet AND b.objet=\'article\' AND b.id_auteur=c.id_auteur)',
+		'a.id_rubrique=' . intval($rub_parent)
+	);
+	foreach ($articles as $a) {
+		$prologues[$a['id_article']] = $a['titre'] . ' par : ' . $a['nom'];
+		$defaut[] = $a['id_article'];
+	}
 	$saisies = [
-		'saisie' => 'fieldset',
+		'saisie' => 'checkbox',
 		'options' => [
-			'nom' => 'conci_info'
-		],
-		'saisies' => [
-			[
-				'saisie' => 'input',
-				'options' => [
-					'nom' => 'nbr_histoire',
-					'label' => _T('petitfablab:creer_histoire_nbr'),
-					'obligatoire' => 'oui',
-					'type' => 'number',
-					'step' => '1',
-					'min' => '1',
-					'defaut' => '1'
-				],
-				'verifier' => [
-					'type' => 'entier',
-					'options' => [
-						'min' => 1
-					]
-				]
-			]
+			'nom' => 'prologue',
+			'label' => _T('petitfablab:creer_histoire_prologue'),
+			'obligatoire' => 'oui',
+			'data' => $prologues,
+			'defaut' => $defaut
 		]
 	];
 
@@ -44,13 +38,12 @@ function formulaires_creer_histoire_saisies_dist($rub_parent, $retour = '') {
 // Enregistrement des données
 function formulaires_creer_histoire_traiter_dist($rub_parent, $retour = '') {
 
-	$nbr_histoire = _request('nbr_histoire');
-	$i = 0;
-	while ($i <= $nbr_histoire) {
-		$id_prologue = sql_getfetsel("id_article", "spip_articles", ['titre LIKE "%prologue%"', 'statut="publie"', 'id_rubrique =' . intval($rub_parent)], "", "RAND()");
-
+	$prologues = _request('prologue');
+	$rub = [];
+	foreach ($prologues as $id_prologue) {
 		// Rubrique
 		$id_rub = rubrique_inserer($rub_parent);
+		$rub[] = $id_rub;
 		autoriser_exception('modifier', 'rubrique', $id_rub);
 		objet_modifier('rubrique', $id_rub, ["titre" => "Histoire " . $id_rub, "descriptif" => $id_prologue]);
 		objet_instituer('rubrique', $id_rub, ['id_parent' => $rub_parent, 'statut' => 'publie']);
@@ -66,11 +59,10 @@ function formulaires_creer_histoire_traiter_dist($rub_parent, $retour = '') {
 
 		// Cloture exceptions
 		autoriser_exception('modifier', 'rubrique', $id_rub, false);
-
-		$i++;
 	}
 
 	// Recharger la page
+	$retour = parametre_url($retour, 'rub', $rub);
 	$res['redirect'] = $retour;
 	return $res;
 }
