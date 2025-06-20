7c5b857e44bda92f7d5d443374ac2af73a4c89c5
Pierre KUHN <webmaster@pierre-kuhn.org>
2025-06-20 13:35:55 +0200

    feat: maj de la config autorite

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -138,12 +138,14 @@ if [ ${PROJET} != "laclasse" ]; then
 fi
 spip plugins:maj:bdd
 
+spip config:ecrire -p autorite editer_forums:1
+spip config:ecrire -p autorite auteur_mod_email:0
+spip config:ecrire -p autorite auteur_modere_forum:0
 spip config:ecrire -p bigup charger_public:1
 spip config:ecrire -p bigup max_file_size:${PHP_UPLOAD_MAX_FILESIZE%M}
 spip config:ecrire -p mediabox active:oui
 spip config:ecrire -p notation acces:ide
 spip config:ecrire -p notation change_note:oui
-spip config:ecrire -p autorite editer_forums:1
 
 # Default mes_options
 rm -rf config/mes_options.php
