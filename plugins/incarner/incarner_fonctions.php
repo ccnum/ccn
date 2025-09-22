69c47f6d353f9bde243655535e839b79bf8d23ef
DDEV User <nobody@example.com>
2025-09-22 10:11:06 +0200

    Mise à jour du plugin 'incarner' : v3.0.0

--- plugins/incarner/incarner_fonctions.php
+++ plugins/incarner/incarner_fonctions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Fonctions utiles au plugin Incarner
  *
@@ -18,16 +19,14 @@ if (! defined('_INCARNER_DELAI_EXPIRATION')) {
 
 /**
  * Determiner si on affiche le login ou l'email
- * @param $login
- * @param $email
  * @return mixed
  */
 function incarner_login_affiche($login, $email) {
-	$login_aff = $login ? $login : $email;
+	$login_aff = $login ?: $email;
 	// si le login est un md5 et qu'on a un email, afficher l'email
 	if (strlen($login_aff) == 32
-		and !preg_match(",[^0-9a-f],i", $login_aff)
-	    and $email) {
+		and !preg_match(',[^0-9a-f],i', $login_aff)
+		and $email) {
 		$login_aff = $email;
 	}
 	return $login_aff;
@@ -48,11 +47,11 @@ function incarner_cle_valide($cle) {
 		return false;
 	}
 
-	list($hash, $id_auteur_session_origine, $id_auteur_incarne) = $cle_parts;
+	[$hash, $id_auteur_session_origine, $id_auteur_incarne] = $cle_parts;
 
 	// si l'auteur connecte n'est pas parmi l'un des 2 id_auteur c'est une suspicion d'usurpation
 	if (empty($GLOBALS['visiteur_session']['id_auteur'])
-	  or !in_array($GLOBALS['visiteur_session']['id_auteur'], array($id_auteur_session_origine, $id_auteur_incarne))) {
+	  or !in_array($GLOBALS['visiteur_session']['id_auteur'], [$id_auteur_session_origine, $id_auteur_incarne])) {
 		incarner_invalider_cle($cle);
 		return false;
 	}
@@ -60,15 +59,15 @@ function incarner_cle_valide($cle) {
 	include_spip('inc/config');
 
 	if (! $cles = lire_config('incarner/cles')) {
-		$cles = array();
+		$cles = [];
 	}
 	if (! $maj = lire_config('incarner/maj')) {
-		$maj = array();
+		$maj = [];
 	}
 
 	$now = time();
 	if ($cles and $cles[$id_auteur_session_origine] === $cle) {
-		if (empty($maj[$id_auteur_session_origine]) or ($now > ($maj[$id_auteur_session_origine] + _INCARNER_DELAI_EXPIRATION))){
+		if (empty($maj[$id_auteur_session_origine]) or ($now > ($maj[$id_auteur_session_origine] + _INCARNER_DELAI_EXPIRATION))) {
 			// la cle est expiree
 			incarner_invalider_cle($cle);
 			return false;
@@ -94,7 +93,6 @@ function incarner_cle_valide($cle) {
  *
  * @param int $id_auteur_session
  * @param int $id_auteur_incarne
- * @throws Exception
  */
 function incarner_renouveler_cle($id_auteur_session, $id_auteur_incarne) {
 
@@ -108,18 +106,14 @@ function incarner_renouveler_cle($id_auteur_session, $id_auteur_incarne) {
 	}
 
 	if (! $cles = lire_config('incarner/cles')) {
-		$cles = array();
+		$cles = [];
 	}
 	if (! $maj = lire_config('incarner/maj')) {
-		$maj = array();
+		$maj = [];
 	}
 
 	include_spip('inc/acces');
-	$cle_parts = array(
-		md5(creer_uniqid()),
-		intval($id_auteur_session),
-		intval($id_auteur_incarne)
-	);
+	$cle_parts = [md5(creer_uniqid()), intval($id_auteur_session), intval($id_auteur_incarne)];
 
 	$nouvelle_cle = encoder_contexte_ajax($cle_parts, 'incarner');
 
@@ -132,19 +126,17 @@ function incarner_renouveler_cle($id_auteur_session, $id_auteur_incarne) {
 	spip_setcookie(
 		'spip_cle_incarner',
 		$nouvelle_cle,
-		defined('_INCARNER_COOKIE_PERSISTANT') ? time()+intval(_INCARNER_COOKIE_PERSISTANT) : 0
+		defined('_INCARNER_COOKIE_PERSISTANT') ? time() + intval(_INCARNER_COOKIE_PERSISTANT) : 0
 	);
 }
 
 /**
  * Invalider la clé de l'auteur et effacer son cookie
- *
- * @return null
  */
 function incarner_invalider_cle($cle = null) {
 
-	if (is_null($cle)) {
-		$cle = (isset($_COOKIE['spip_cle_incarner']) ? $_COOKIE['spip_cle_incarner'] : '');
+	if ($cle === null) {
+		$cle = ($_COOKIE['spip_cle_incarner'] ?? '');
 	}
 
 	if ($cle) {
@@ -168,7 +160,6 @@ function incarner_invalider_cle($cle = null) {
 	}
 }
 
-
 /**
  * Envoyer un mail de notification par sécurité
  * @param array $auteur
@@ -180,19 +171,19 @@ function incarner_notifier_nouvelle_incarnation($auteur) {
 
 		$auteur_source =
 			incarner_login_affiche($GLOBALS['visiteur_session']['login'], $GLOBALS['visiteur_session']['email'])
-			." #".$GLOBALS['visiteur_session']['id_auteur'];
+			. ' #' . $GLOBALS['visiteur_session']['id_auteur'];
 
 		$auteur_dest =
 			incarner_login_affiche($auteur['login'], $auteur['email'])
-			." #".$auteur['id_auteur'];
+			. ' #' . $auteur['id_auteur'];
 
-		$args = array('auteur_source' => $auteur_source, 'auteur_dest' => $auteur_dest);
+		$args = ['auteur_source' => $auteur_source, 'auteur_dest' => $auteur_dest];
 		$sujet = _T('incarner:notification_sujet_email', $args);
 		$texte = _T('incarner:notification_texte_email', $args)
 			. "\n\n"
-			. _T('date') .' : ' . date('Y-m-d H:i:s')
+			. _T('date') . ' : ' . date('Y-m-d H:i:s')
 			. "\n"
-			. "IP :" . $GLOBALS['ip'];
+			. 'IP :' . $GLOBALS['ip'];
 
 		$envoyer_mail = charger_fonction('envoyer_mail', 'inc');
 		$envoyer_mail($email_dest, $sujet, $texte);
