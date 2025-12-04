5e75b972adc6e14d422ef6c5902eb8631f28817b
pierretux <pierrekuhn@ik.me>
2025-12-04 14:53:58 +0100

    feat(fictions): refonte du code pour la mise en ligne des articles

--- plugins/fictions/fictions_pipelines.php
+++ plugins/fictions/fictions_pipelines.php
@@ -12,3 +12,25 @@ function fictions_cicas($flux) {
 
 	return $flux;
 }
+
+function fictions_post_edition($flux) {
+	spip_log($flux['args'], 'test');
+	if (
+		$flux['args']['action'] == 'modifier'
+		and !isset($flux['args']['data'])
+		and $table = $flux['args']['table']
+		and $id_objet = $flux['args']['id_objet']
+		and $flux['args']['champs_anciens']['statut'] == 'prop'
+		and $table = 'spip_articles'
+		and $id_rubrique = $flux['args']['champs_anciens']['id_rubrique']
+	) {
+		// Publier l'article que l'on vient de modifier
+		sql_updateq($table, ['statut' => 'publie'], 'id_article=' . intval($id_objet));
+		// Passer de prépa à prop le suivant
+		$id_article = sql_getfetsel('id_article', 'spip_articles', ['id_rubrique=' . intval($id_rubrique), 'statut=' . sql_quote('prepa')], '', 'id_article', '0,1');
+		if ($id_article) {
+			sql_updateq($table, ['statut' => 'prop'], 'id_article=' . intval($id_article));
+		}
+	}
+	return $flux;
+}
