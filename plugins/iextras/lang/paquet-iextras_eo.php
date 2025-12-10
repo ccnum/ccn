5e7a2d31f034b2ab97f1543fb5d61316fca74e5f
DDEV User <nobody@example.com>
2025-12-10 10:35:55 +0100

    Ajout du plugin 'iextras' : v4.3.0

---
+++ plugins/iextras/lang/paquet-iextras_eo.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-iextras?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+return [
+
+	// I
+	'iextras_description' => 'Aldonas en la privata spaco kompletan interfacon por mastrumi ekstrajn kampojn en redaktaj objektoj.',
+	'iextras_nom' => 'Ekstraj kampoj (interfaco)',
+	'iextras_slogan' => 'Disponigas grafikan interfacon por mastrumi ekstrajn kampojn',
+	'iextras_titre' => 'Ekstraj kampoj (interfaco)',
+];
---
+++ plugins/iextras/lang/paquet-iextras_it.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-iextras?lang_cible=it
+// ** ne pas modifier le fichier **
+
+return [
+
+	// I
+	'iextras_description' => 'Aggiunge nell’area riservata un’interfaccia completa per la gestione di campi aggiuntivi negli oggetti editoriali.',
+	'iextras_nom' => 'Campi Extra (Interfaccia)',
+	'iextras_slogan' => 'Interfaccia per gestire i campi extra',
+	'iextras_titre' => 'Campi Extra (Interfaccia)',
+];
