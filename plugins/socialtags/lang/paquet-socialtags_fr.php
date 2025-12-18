699aaab85cbd48ca98c118d62cf55f8aa555e542
DDEV User <nobody@example.com>
2025-12-18 16:03:53 +0100

    Mise à jour du plugin 'socialtags' : v4.2.0

--- plugins/socialtags/lang/paquet-socialtags_fr.php
+++ plugins/socialtags/lang/paquet-socialtags_fr.php
@@ -1,14 +1,11 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 // Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/socialtags.git
-if (!defined('_ECRIRE_INC_VERSION')) {
-	return;
-}
 
-$GLOBALS[$GLOBALS['idx_lang']] = array(
+return [
 
 	// S
 	'socialtags_description' => 'Le plugin Social Tags permet d’ajouter des icônes de partage de liens vers les sites tels que Digg, Facebook, Delicious...
 Il propose également des noisettes paramétrables permettant d’afficher les badges et boites de type "J’aime" de Facebook.',
-	'socialtags_slogan' => 'Activer des boutons de partage de liens sur les réseaux sociaux.'
-);
+	'socialtags_slogan' => 'Activer des boutons de partage de liens sur les réseaux sociaux.',
+];
