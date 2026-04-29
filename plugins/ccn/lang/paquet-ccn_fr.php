7f7ae9e88d4cc7a1f4deb4299abe35abb840ccc0
pierretux <pierrekuhn@ik.me>
2026-04-29 07:40:21 +0200

    feat(ccn): déplacer la description du paquet.xml vers le fichier de lang

---
+++ plugins/ccn/lang/paquet-ccn_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+return [
+
+	'ccn_titre' => 'CCN',
+	'ccn_description' => "Plugin commun aux sites CCN — gestion de l'année scolaire partagée entre thematique, fictions et petitfablab"
+];
