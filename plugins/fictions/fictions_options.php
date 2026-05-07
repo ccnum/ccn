4db42d2c0ae4232dbfe07dd51957584abb1218e9
pierretux <pierrekuhn@ik.me>
2026-05-07 12:49:09 +0200

    fix(fictions): corriger bugs, refactorer JS et améliorer accessibilité - Pipeline: corriger comparaison string/int (=== -> (int)$blog) sur Blog Pédagogique - Masquage: remplacer regex [[:alpha:]] par masquerTexteChapitre() pour gérer les accents - Supprimer balise_NOM_AUTEUR_dist (nom auteur hardcodé) et cleanCut() (doublon) - Centraliser l'ID rubrique Blog Pédagogique dans la constante _FICTIONS_ID_BLOG_PEDA (6 templates) - Extraire resize_global_content(), pagination(), next_prev() vers script.js - Nettoyer JS inline: supprimer code mort (#script-forum-fleche-roll), remplacer   les url(#CHEMIN{}) par toggleClass et convention _hover.png - Ajouter attributs alt manquants sur toutes les images de navigation - CSS: ajouter règles pour #close-script-ecrivain et .is-open (bt-moins.png)

--- plugins/fictions/fictions_options.php
+++ plugins/fictions/fictions_options.php
@@ -5,3 +5,7 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 // _annee_scolaire, _date_debut, _date_fin sont définis par le plugin ccn (ccn_options.php)
+
+if (!defined('_FICTIONS_ID_BLOG_PEDA')) {
+	define('_FICTIONS_ID_BLOG_PEDA', 12);
+}
--- plugins/fictions/squelettes/js/script.js
+++ plugins/fictions/squelettes/js/script.js
@@ -1,3 +1,34 @@
 function confirmation(txt) {
     return window.confirm(txt);
 }
+
+function resize_global_content() {
+    var h = $(window).height() - 61;
+    $('.global-content').height(h);
+    var stitches_margin = ((h - 60) - 258) / 4;
+    $('.stitches-inner').css({'margin-top': stitches_margin});
+}
+
+function pagination(var_compteur) {
+    var num_page_gauche, num_page_droite;
+    if (var_compteur % 2 === 0) {
+        num_page_gauche = var_compteur - 1;
+        num_page_droite = var_compteur;
+    } else {
+        num_page_gauche = var_compteur;
+        num_page_droite = var_compteur + 1;
+    }
+    $('.lecture-page').hide();
+    $('#page-gauche-' + num_page_gauche).show();
+    $('#page-droite-' + num_page_droite).show();
+}
+
+function next_prev(var_compteur, var_limite) {
+    $('#bt-next, #bt-prev').show();
+    if (var_compteur >= var_limite - 1) {
+        $('#bt-next').hide();
+    }
+    if (var_compteur === 1 || var_compteur === 2) {
+        $('#bt-prev').hide();
+    }
+}
