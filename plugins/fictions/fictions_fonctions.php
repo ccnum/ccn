ed12ad9cd1bbf395ef1e3f30907bccdbf2d37e0b
pierretux <pierrekuhn@ik.me>
2026-05-07 12:49:09 +0200

    fix(fictions): corriger bugs, refactorer JS et améliorer accessibilité - Pipeline: corriger comparaison string/int (=== -> (int)$blog) sur Blog Pédagogique - Masquage: remplacer regex [[:alpha:]] par masquerTexteChapitre() pour gérer les accents - Supprimer balise_NOM_AUTEUR_dist (nom auteur hardcodé) et cleanCut() (doublon) - Centraliser l'ID rubrique Blog Pédagogique dans la constante _FICTIONS_ID_BLOG_PEDA (6 templates) - Extraire resize_global_content(), pagination(), next_prev() vers script.js - Nettoyer JS inline: supprimer code mort (#script-forum-fleche-roll), remplacer   les url(#CHEMIN{}) par toggleClass et convention _hover.png - Ajouter attributs alt manquants sur toutes les images de navigation - CSS: ajouter règles pour #close-script-ecrivain et .is-open (bt-moins.png)

--- plugins/fictions/fictions_fonctions.php
+++ plugins/fictions/fictions_fonctions.php
@@ -6,11 +6,6 @@
 include_spip('action/editer_objet');
 include_spip('base/abstract_sql');
 
-function balise_NOM_AUTEUR_dist($p) {
-    $p->code = "'Violaine Schwartz'";
-    return $p;
-}
-
 // Si balise_FIN_dist = false -> affichage de la grille sur la page d'accueil
 // Si balise_FIN_dist = true -> affichage des couvertures et liens pdf sur la page d'accueil
 
@@ -27,39 +22,12 @@ function balise_LECTURE_dist($p) {
     return $p;
 }
 
-// FUNCTION CLEANCUT
-function cleanCut($string, $length = 380, $cutString = '(...)') {
-    // Si la chaîne est plus courte que la longueur maximale, on la retourne telle quelle
-    if (mb_strlen($string) <= $length) {
-        return $string;
-    }
-
-    // On prend la fin de la chaîne en commençant à la position calculée
-    $start = mb_strlen($string) - $length + mb_strlen($cutString);
-    $str = mb_substr($string, max(0, $start));
-
-    // On cherche le premier espace pour couper proprement
-    $pos = mb_strpos($str, ' ');
-
-    // Si on trouve un espace, on coupe à cet endroit, sinon on prend toute la fin
-    if ($pos !== false) {
-        $result = $cutString . mb_substr($str, $pos);
-    } else {
-        $result = $cutString . $str;
-    }
-
-    return $result;
-}
-
 /**
- * Reçoit un chapitre complet et masque les X derniers caractères en remplaçant
- * toutes les lettres (y compris les caractères accentués) par 'X'.
+ * Masque les $nbDeCaracteresATronquerALaFin derniers caractères d'un texte
+ * en remplaçant toutes les lettres (Unicode) par 'X'. Retourne une chaîne vide
+ * si le texte est plus court que le seuil.
  *
- * Si le chapitre contient moins de caractères que le nb à tronquer, retourne une chaîne vide.
- *
- * @param  string $texteAMasquer
- * @param  int    $nbDeCaracteresATronquerALaFin
- * @return string
+ * Utilisable comme filtre SPIP : [(#DESCRIPTIF|textebrut|masquerTexteChapitre{400})]
  */
 function masquerTexteChapitre(string $texteAMasquer = '', int $nbDeCaracteresATronquerALaFin = 325): string {
     if (mb_strlen($texteAMasquer) < $nbDeCaracteresATronquerALaFin) {
@@ -71,19 +39,11 @@ function masquerTexteChapitre(string $texteAMasquer = '', int $nbDeCaracteresATr
 }
 
 /**
- * Cette fonction reçoit une chaîne de caractères (un chapitre) et doit n'en retourner que les X derniers caractères. X
- * étant le nombre de caractères finaux que nous désirons. Enfin avant de renvoyer la fin de chaîne ainsi tronquée, on
- * lui concatènera (à son commencement) la chaîne de caractère éventuelle reçue en troisième argument (qui est sensée
- * symboliser le texte manquant)
+ * Retourne les $nbDeDerniersCaracteresAAfficher derniers caractères d'un texte,
+ * précédés de $chaineAConcatenerAuDebut. Retourne le texte complet si sa longueur
+ * est inférieure au seuil.
  *
- * Ex : recupererDernieresLignesChapitres('toto_titi_tutu_tata', 5, '...') renverra ...titi_tutu_tata
- *
- * -> Si le nombre de caractères voulus est supérieur à la taille du texte, on renverra le texte complet sans la chaîne
- * à concaténer au début.
- *
- * @param  string $texteChapitre
- * @param  int    $nbDeDerniersCaracteresAAfficher
- * @return string
+ * Utilisable comme filtre SPIP : [(#DESCRIPTIF|textebrut|recupererDernieresLignesChapitres{400})]
  */
 function recupererDernieresLignesChapitres(string $texteChapitre = '', int $nbDeDerniersCaracteresAAfficher = 325, string $chaineAConcatenerAuDebut = '(...)'): string {
     if (mb_strlen($texteChapitre) < $nbDeDerniersCaracteresAAfficher) {
@@ -91,7 +51,6 @@ function recupererDernieresLignesChapitres(string $texteChapitre = '', int $nbDe
     }
     return $chaineAConcatenerAuDebut . mb_substr($texteChapitre, -$nbDeDerniersCaracteresAAfficher);
 }
-
 
 function anneeAAfficher($derniereRubriqueAnneeTrouveeDansSpip = '') {
     if (isset($_GET['annee_scolaire'])) {
