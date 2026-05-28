a3d6b20257390990904c2590e7c7e3c6e1b570bb
antoineDerooGrandLyon <ext.sully.aderoo@grandlyon.com>
2026-05-28 10:03:11 +0200

    éviter de déclarer ajaxbloc_selecteur plusieurs fois

--- plugins/thematique/squelettes/js/layout.js
+++ plugins/thematique/squelettes/js/layout.js
@@ -2,7 +2,11 @@
 // Fonctions chargées dans le layout (différentes des fonctions du projet)
 ////////////////////////////////////////////////////////////////
 
-const ajaxbloc_selecteur = '.pagination a, a.ajax, a.lienss, .titre_article a';
+(function(){
+	if (typeof window !== 'undefined') {
+		window.ajaxbloc_selecteur = window.ajaxbloc_selecteur || '.pagination a, a.ajax, a.lienss, .titre_article a';
+	}
+})();
 
 function confirmation(txt) {
 	return window.confirm(txt);
