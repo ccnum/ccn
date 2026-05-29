5473f3aaee3f1f934b1e71492c53be3dca0a0e0a
pierretux <pierrekuhn@ik.me>
2026-05-29 14:54:49 +0200

    fix(security): F2 remplacer innerHTML par DOM API dans bouton.js, F4 supprimer commentaires debug XML

--- plugins/thematique/squelettes/js/bouton.js
+++ plugins/thematique/squelettes/js/bouton.js
@@ -14,7 +14,15 @@ function bouton() {
 			case 0:
 				this.div_base = document.createElement("div");
 				if ((CCN.idRestreint > 0) && (CCN.typeRestreint != '') && (CCN.typeRestreint != 'travail_en_cours')) {
-					this.div_base.innerHTML = "<div id='reponse_plus2' class='bouton_article_plus'><img src='" + CCN.urlRoot + "img/reponse_plus.png' onClick='createReponse(false,0, " + CCN.idRestreint + ");' title='publier un nouvel article'></div>";
+					const wrapper0 = document.createElement("div");
+					wrapper0.id = "reponse_plus2";
+					wrapper0.className = "bouton_article_plus";
+					const img0 = document.createElement("img");
+					img0.src = CCN.urlRoot + "img/reponse_plus.png";
+					img0.title = "publier un nouvel article";
+					img0.addEventListener("click", () => createReponse(false, 0, CCN.idRestreint));
+					wrapper0.appendChild(img0);
+					this.div_base.appendChild(wrapper0);
 					this.div_base.style.position = "absolute";
 					this.div_base.style.left = "195px";
 					this.div_base.style.bottom = "-105px";
@@ -25,7 +33,15 @@ function bouton() {
 			case 1:
 				this.div_base = document.createElement("div");
 				if ((CCN.idRestreint > 0) && (CCN.typeRestreint != '') && (CCN.typeRestreint != 'travail_en_cours')) {
-					this.div_base.innerHTML = "<div id='reponse_plus2' class='bouton_article_plus'><img src='" + CCN.urlRoot + "img/reponse_plus.png' onClick='createReponse(false,0, " + CCN.idRestreint + ");' title='publier un nouvel article'></div>";
+					const wrapper1 = document.createElement("div");
+					wrapper1.id = "reponse_plus2";
+					wrapper1.className = "bouton_article_plus";
+					const img1 = document.createElement("img");
+					img1.src = CCN.urlRoot + "img/reponse_plus.png";
+					img1.title = "publier un nouvel article";
+					img1.addEventListener("click", () => createReponse(false, 0, CCN.idRestreint));
+					wrapper1.appendChild(img1);
+					this.div_base.appendChild(wrapper1);
 					this.div_base.style.position = "absolute";
 					this.div_base.style.left = "20px";
 					this.div_base.style.bottom = "60px";
