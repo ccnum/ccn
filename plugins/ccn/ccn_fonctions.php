b1ef8b309e95b0343524e6b34edae4d03e397bbf
pierretux <pierrekuhn@ik.me>
2026-05-31 13:23:26 +0200

    fix: supprimer annee_rub1() inutilisée et buguée
    La fonction lisait id_rubrique au lieu de la date maj,
    rendant le calcul d'année scolaire incorrect.
    Ferme #268.

--- plugins/ccn/ccn_fonctions.php
+++ plugins/ccn/ccn_fonctions.php
@@ -17,19 +17,6 @@ function annee_rub($idr) {
 	return $annee_scolaire;
 }
 
-function annee_rub1($idr) {
-	$annee_scolaire = 0;
-	$date = sql_getfetsel('id_rubrique', 'spip_rubriques', 'id_rubrique=' . intval($idr));
-	if ($date != '') {
-		$annee_scolaire = intval(substr($date, 0, 4));
-		$mois_scolaire = intval(substr($date, 5, 2));
-		if ($mois_scolaire < 9) {
-			$annee_scolaire--;
-		}
-	}
-	return $annee_scolaire;
-}
-
 function balise_ANNEE_SCOLAIRE_dist($p) {
 	// _annee_scolaire est calculé et validé dans ccn_options.php
 	$p->code = '_annee_scolaire';
