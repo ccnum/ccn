0081e7042927f335e1b0c9d3aa4c58952b2ac1b5
pierretux <pierrekuhn@ik.me>
2026-07-21 08:27:37 +0200

    fix(docker-entrypoint): convertit correctement PHP_UPLOAD_MAX_FILESIZE (Go/Mo/Ko) en Mo pour bigup

---
+++ plugins/thematique/formulaires/public_publier_article.php
@@ -0,0 +1,91 @@
+<?php
+
+/**************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2010                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+
+function formulaires_public_publier_article_traiter_dist(
+    $id_rubrique,
+    $id_consigne = 0
+) {
+    include_spip('inc/editer');
+    include_spip('prive/formulaires/editer_article');
+
+    $res = formulaires_editer_objet_traiter(
+        'article',
+        'new',
+        $id_rubrique
+    );
+
+    if (empty($res['erreurs']) && !empty($res['id_article'])) {
+
+        if ($id_consigne) {
+            include_spip('action/editer_objet');
+
+            objet_modifier(
+                'article',
+                $res['id_article'],
+                [
+                    'id_consigne' => $id_consigne
+                ]
+            );
+        }
+
+        $res['message_ok'] = "Article publié !";
+        $res['redirect'] = generer_url_public(
+            'article',
+            'id_article=' . $res['id_article'] . '&mode=complet'
+        );
+    }
+
+    return $res;
+}
+
+
+
+function formulaires_public_publier_article_charger_dist(
+    $id_rubrique,
+    $id_consigne = 0
+) {
+    include_spip('inc/editer');
+    include_spip('prive/formulaires/editer_article');
+
+    $valeurs = formulaires_editer_objet_charger(
+        'article',
+        'new',
+        $id_rubrique
+    );
+
+    $valeurs['id_consigne'] = $id_consigne;
+
+    return $valeurs;
+}
+
+function formulaires_public_publier_article_verifier_dist(
+    $id_rubrique,
+    $id_consigne = 0
+) {
+    include_spip('inc/editer');
+    include_spip('prive/formulaires/editer_article');
+
+    return formulaires_editer_objet_verifier(
+        'article',
+        'new',
+        [
+            'titre',
+            'texte'
+        ]
+    );
+}
