8442f159d5759bfd12ca07cd0cdb4a618f0ddf14
pierretux <pierrekuhn@ik.me>
2026-09-25 17:47:08 +0200

    fix(fictionsv2): masquer automatiquement le chapitre précédent à la publication (#219)

--- plugins/projets/fictionsv2/fictionsv2_pipelines.php
+++ plugins/projets/fictionsv2/fictionsv2_pipelines.php
@@ -29,6 +29,7 @@ function fictionsv2_post_edition($flux) {
 	}
 
 	include_spip('action/editer_objet');
+	include_spip('fictionsv2/fictionsv2_fonctions');
 
 	$blog = fictionsv2_id_rubrique_a_mot('blog_pedagogique');
 	// #229 : la rubrique blog auteur suit la même mécanique de publication automatique
@@ -38,6 +39,16 @@ function fictionsv2_post_edition($flux) {
 	if ($statut === 'prop') {
 		// Publier l'article que l'on vient de modifier
 		objet_modifier('article', $id_objet, ['statut' => 'publie']);
+		// Masquer le chapitre précédent en tronquant son descriptif (#219)
+		$prev_id = sql_getfetsel('id_article', 'spip_articles', [
+			'id_rubrique=' . intval($id_rubrique),
+			'statut=' . sql_quote('publie'),
+			'id_article<>' . intval($id_objet),
+		], '', 'id_article', '1', 'id_article DESC');
+		if ($prev_id) {
+			$prev_desc = sql_getfetsel('descriptif', 'spip_articles', 'id_article=' . intval($prev_id));
+			objet_modifier('article', intval($prev_id), ['descriptif' => masquerTexteChapitre($prev_desc)]);
+		}
 		// Passer de prépa à prop le suivant
 		$id_article = sql_getfetsel('id_article', 'spip_articles', ['id_rubrique=' . $id_rubrique, 'statut=' . sql_quote('prepa')], '', 'id_article', '0,1');
 		if ($id_article) {
@@ -47,6 +58,16 @@ function fictionsv2_post_edition($flux) {
 
 	if (in_array($id_rubrique, $blogs_ids, true) && $statut === 'prepa') {
 		objet_modifier('article', $id_objet, ['statut' => 'publie']);
+		// Masquer le chapitre précédent en tronquant son descriptif (#219)
+		$prev_id = sql_getfetsel('id_article', 'spip_articles', [
+			'id_rubrique=' . intval($id_rubrique),
+			'statut=' . sql_quote('publie'),
+			'id_article<>' . intval($id_objet),
+		], '', 'id_article', '1', 'id_article DESC');
+		if ($prev_id) {
+			$prev_desc = sql_getfetsel('descriptif', 'spip_articles', 'id_article=' . intval($prev_id));
+			objet_modifier('article', intval($prev_id), ['descriptif' => masquerTexteChapitre($prev_desc)]);
+		}
 	}
 
 	return $flux;
